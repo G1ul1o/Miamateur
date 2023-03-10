f210505a97208f06708e41ffd4db440fcdad87b5
Maxime <maxime.boulle@efrei.net>
2023-03-10 19:36:03 +0100

    menu nav

---
+++ script.js
@@ -0,0 +1,13 @@
+function changeDisplay(){
+    var nav = document.getElementById("nav");
+    var header = document.getElementById("header");
+    if(nav.style.display == "flex"){
+        nav.style="display: none";
+        header.style="box-shadow: 10px 0 20px 0 rgba(0,0,0,0.5);";
+    }else {
+        nav.style="display: flex";
+        header.style="box-shadow: none";
+
+    }
+    console.log(nav.style);
+}
