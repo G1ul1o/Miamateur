f46cc14f00178806cad9f1e2d7dada9f5052c8a0
MaximeBoulle <104087634+MaximeBoulle@users.noreply.github.com>
2023-05-22 17:40:00 +0200

    Update connection.php

--- PHP/connection.php
+++ PHP/connection.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$dbname = "miamateur";
-$password = "Boulle1203";
+$host = 'localhost';
+$dbname = 'id20532041_miamateur';
+$username = 'id20532041_root';
+$password = 'geGt8T)]JgI]zzZY';
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
