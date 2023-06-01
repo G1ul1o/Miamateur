77a64eede477bfd52a802da54f410187fed156ea
Maxime <maxime.boulle@efrei.net>
2023-06-01 23:13:42 +0200

    header rectification

--- HTML/formulaire.php
+++ HTML/formulaire.php
@@ -10,6 +10,7 @@ include '../PHP/connection.php';
     <title>Formulaire</title>
     <script src="../JS/index.js" defer></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../JS/formulaire.js" defer></script>
     <link rel="stylesheet" href="../CSS/formulaire.css">
     <link rel="stylesheet" href="../CSS/HEADER.css">
     <link rel="stylesheet" href="../CSS/FOOTER.css">
@@ -48,7 +49,7 @@ include '../PHP/connection.php';
     <div class="alignement_cote">
         <div class="alignement">
             <!-- les boutons du header (Acceuil, notre selection...) -->
-            <a class="active header-button" href="../index.php" target="_self">Accueil</a>
+            <a class="header-button" href="../index.php" target="_self">Accueil</a>
 
             <a class="header-button" href="selection.php" target="_self">Notre selection</a>
 
@@ -61,53 +62,57 @@ include '../PHP/connection.php';
     </div>
 </header>
     <main>
-    <h1>Ajouter une recette</h1>
+        <h1>Ajouter une recette</h1>
 
-    <form method="POST" action="traitement.php" enctype="multipart/form-data">
+        <form> <!--action="traitement.php" method="post" enctype="multipart/form-data"-->
+            <label for="photo">Photo :</label>
+            <!--<input type="file" id="photo" name="photo" accept="image/*"><br><br>-->
+            <input type="text" id="photo" name="photo"><br><br>
 
-        <label for="nom">Nom de la recette :</label>
-        <input type="text" name="nom" required><br>
+            <label for="nom">Nom du plat :</label>
+            <input type="text" id="nom" name="nom"><br><br>
 
-        <label for="image">Image miniature :</label>
-        <input type="file" name="image"><br>
+            <label for="temps">temps prevue :</label>
+            <input type="number" id="temps" name="temps"
+                   min="1" max="180"><p1>min</p1><br><br>
 
-        <label for="description">Description :</label><br>
-        <textarea name="description"></textarea><br>
 
-        <label for="instructions">Instructions :</label><br>
-        <textarea name="instructions"></textarea><br>
+            <label for="quantite">Quantite prevue :</label>
+            <input type="number" id="quantite" name="quantite"
+                   min="1" max="20"><p1>personne</p1><br><br>
 
-        <label for="temps_preparation">Temps de préparation :</label>
-        <input type="number" name="temps_preparation" min="0" required><br>
+            <label for="tags-container">Tags :</label><br>
+            <div id="tags-container">
+                <div class="tag">
+                    <input type="text" name="tag[]" placeholder="Tag 1">
+                    <button type="button" class="supprimer-tag">Supprimer</button>
+                </div>
+            </div>
+            <button type="button" id="ajouter-tag">Ajouter un Tag</button>
+            <br>
 
-        <label for="temps_cuisson">Temps de cuisson :</label>
-        <input type="number" name="temps_cuisson" min="0" required><br>
+            <label for="description">Description :</label><br>
+            <textarea id="description" name="description" rows="4" cols="50"></textarea><br><br>
 
-        <label for="nb_portions">Nombre de portions :</label>
-        <input type="number" name="nb_portions" min="1" required><br>
+            <label for="ingredients-container">Ingredient :</label>
+            <div id="ingredients-container">
+                <div class="ingredient">
+                    <input type="text" name="ingredient[]" placeholder="Ingredient 1">
+                    <button type="button" class="supprimer-ingredient">Supprimer</button>
+                </div>
+            </div>
+            <button type="button" id="ajouter-ingredient">Ajouter un Ingredient</button>
 
-        <label for="prix">Prix :</label>
-        <input type="number" name="prix" min="0" step="0.01" required><br>
-
-        <label for="tags">Tags :</label><br>
-        <input type="checkbox" name="tags[]" value="vegetarien"> Végétarien<br>
-        <input type="checkbox" name="tags[]" value="sans_gluten"> Sans gluten<br>
-        <input type="checkbox" name="tags[]" value="sans_lactose"> Sans lactose<br>
-        <input type="checkbox" name="tags[]" value="healthy"> Healthy<br>
-
-        <label for="ingredients">Ingrédients :</label><br>
-        <div id="ingredients-container">
-            <input type="text" name="ingredients_nom[]" placeholder="Nom de l'ingrédient" required>
-            <input type="text" name="ingredients_quantite[]" placeholder="Quantité" required>
-            <button type="button" onclick="ajouterIngredient()">Ajouter un ingrédient</button>
-        </div>
-
-        <label for="ustensiles">Ustensiles :</label><br>
-        <div id="ustensiles-container">
-            <input type="text" name="ustensiles_nom[]" placeholder="Nom de l'ustensile" required>
-            <button type="button" onclick="ajouterUstensile()">Ajouter un ustensile</button>
-        </div>
-
+            <br><br>
+            <label for="ustensils-container">Ustensil :</label>
+            <div id="ustensils-container">
+                <div class="ustensil">
+                    <input type="text" name="ustensil[]" placeholder="Ustensil 1">
+                    <button type="button" class="supprimer-ustensil">Supprimer</button>
+                </div>
+            </div>
+            <button type="button" id="ajouter-ustensil">Ajouter un Ustensil</button>
+            <br>
         <input type="submit" value="Ajouter la recette">
 
     </form>
@@ -123,19 +128,19 @@ include '../PHP/connection.php';
             <h2>Nos Réseaux</h2>
             <ul id="social_media">
                 <div class="items_social">
-                    <li><img  src = "image/Instagram-logo.png" alt="insta" ></li>
+                    <li><img  src = "../image/Instagram-logo.png" alt="insta" ></li>
                     <li><a href="#" class="lien_footer_reseau">miamateur</a> </li>
                 </div>
                 <div class="items_social">
-                    <li><img  src = "image/facebook-logo.png" alt="facebook"> </li>
+                    <li><img  src = "../image/facebook-logo.png" alt="facebook"> </li>
                     <li><a href="#" class="lien_footer_reseau">facemateur</a> </li>
                 </div>
                 <div class="items_social">
-                    <li><img src = "image/pinterest-logo.png" alt="pinterest"> </li>
+                    <li><img src = "../image/pinterest-logo.png" alt="pinterest"> </li>
                     <li><a href="#" class="lien_footer_reseau">miamatart</a> </li>
                 </div>
                 <div class="items_social">
-                    <li><img  src = "image/twitter-logo.png" alt="twitter" > </li>
+                    <li><img  src = "../image/twitter-logo.png" alt="twitter" > </li>
                     <li><a href="#" class="lien_footer_reseau">miamaster</a> </li>
                 </div>
             </ul>
